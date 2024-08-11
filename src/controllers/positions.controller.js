785ef78fd5ba58b70289c4f8a6d048525e8b2e72
tablonimus <lautamartin1995@gmail.com>
2024-08-11 02:15:58 -0300

    postition general

--- src/controllers/positions.controller.js
+++ src/controllers/positions.controller.js
@@ -1,4 +1,5 @@
 import Position from "../models/positions.model.js";
+import PositionsGeneral from "../models/positionsGeneral.model.js";
 
 export const getPositions = async (req, res) => {
   try {
@@ -6,6 +7,18 @@ export const getPositions = async (req, res) => {
     console.log(category);
 
     const positions = await Position.findOne({ category: category });
+
+    res.json(positions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: "Error", message: error.message });
+  }
+};
+export const getGeneralPositions = async (req, res) => {
+  try {
+    const { category = "A1" } = req.query;
+
+    const positions = await PositionsGeneral.findOne({ category: category });
 
     res.json(positions);
   } catch (error) {
@@ -34,6 +47,32 @@ export const createPosition = async (req, res) => {
       res.json(position);
     } else {
       const newPosition = await Position.create({
+        image: image,
+        category: category,
+      });
+      console.log("NEW POS", newPosition);
+      res.json(newPosition);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: "Error", message: error.message });
+  }
+};
+export const createGeneralPosition = async (req, res) => {
+  try {
+    const { image, category } = req.body;
+
+    const position = await PositionsGeneral.findOneAndUpdate(
+      { category: category },
+      { image: image, category: category },
+      { new: true }
+    );
+    console.log(position);
+    if (position) {
+      console.log("UPDATING POS", position);
+      res.json(position);
+    } else {
+      const newPosition = await PositionsGeneral.create({
         image: image,
         category: category,
       });
