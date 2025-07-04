1dad6f2123ffa8a1990a170c29af6f7012a03937
tablonimus <lautamartin1995@gmail.com>
2025-07-04 02:39:55 -0300

    fixtures

--- src/routes/fixture.routes.js
+++ src/routes/fixture.routes.js
@@ -2,17 +2,19 @@ import { Router } from "express";
 import {
   createFixture,
   deleteFixture,
-  // getFixtureById,
   getFixtures,
+  normalizeAllFixtures,
+  setActiveFixture,
   updateFixture,
 } from "../controllers/fixture.controller.js";
 
 const router = Router();
 
 router.get("/", getFixtures);
-// router.get("/:id", getFixtureById);
 router.post("/", createFixture);
-router.delete("/:id", deleteFixture);
 router.put("/:id", updateFixture);
+router.patch("/:id/activate", setActiveFixture);
+router.delete("/:id", deleteFixture);
+router.post("/normalize", normalizeAllFixtures);
 
 export default router;
