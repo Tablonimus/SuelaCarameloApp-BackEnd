f0612d2799ed755e8f9f45299c3f3e7b57002024
tablonimus <lautaro.martin@soslinux.com.ar>
2026-05-02 19:07:46 -0300

    feat(controllers): implement pagination and advanced filtering for players
    Update the `getPlayers` controller to support pagination using `page` and `limit` query parameters. Added support for a `searchTerm` using regex for name searches and a `team` filter for club names. The response now includes a `meta` object containing pagination details such as `totalItems`, `totalPages`, and `currentPage`. Improved error handling by returning a 500 status code on failure.

--- src/controllers/players.controller.js
+++ src/controllers/players.controller.js
@@ -1,18 +1,29 @@
 import Player from "../models/players.model.js";
 
 export const getPlayers = async (req, res) => {
-  const { name, category } = req.query;
   try {
-    if (name && category) {
-      const players = await Player.find({ current_club_name: name , category: category});
-      res.json(players);
-    } else {
-      const players = await Player.find();
-      res.json(players);
+    const { page = 1, limit = 10, searchTerm, team } = req.query;
+    const skip = (page - 1) * limit;
+    let filter = {};
+    if (searchTerm) {
+      filter.name = { $regex: searchTerm, $options: 'i' };
     }
+    if (team && team !== 'all') {
+      filter.current_club_name = team;
+    }
+    const totalItems = await Player.countDocuments(filter);
+    const players = await Player.find(filter).skip(skip).limit(parseInt(limit));
+    const totalPages = Math.ceil(totalItems / limit);
+    const meta = {
+      totalItems,
+      totalPages,
+      currentPage: parseInt(page),
+      itemsPerPage: parseInt(limit)
+    };
+    res.json({ data: players, meta });
   } catch (error) {
     console.log(error);
-    res.json(error);
+    res.status(500).json(error);
   }
 };
 
